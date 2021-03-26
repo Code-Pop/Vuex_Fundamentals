0a645118281043f89332f1215d603889ab37ee6d
Andy <andy@Andys-MacBook-Pro.local>
2021-03-26 16:17:28 -0400

    add event

--- tests/unit/EventCard.spec.js
+++ tests/unit/EventCard.spec.js
@@ -3,6 +3,17 @@ import EventCard from '@/components/EventCard'
 
 describe('EventCard', () => {
   it(`renders the event's data successfully`, () => {
-    mount(EventCard)
+    const event = {
+      id: 1,
+      time: '12:00PM',
+      date: 'September 29th, 2022',
+      title: 'Coaching Little League'
+    }
+
+    mount(EventCard, {
+      props: {
+        event
+      }
+    })
   })
 })
