57d26f2626ca83dec1cd944cae0b6d6123db2945
Andy <andy@Andys-MacBook-Pro.local>
2021-04-21 23:32:15 -0400

    factory function

--- tests/unit/EventList.spec.js
+++ tests/unit/EventList.spec.js
@@ -3,36 +3,35 @@ import { mount } from '@vue/test-utils'
 import store from '@/store'
 import router from '@/router'
 
+function mountEventList(config = {}) {
+  config.mountOptions = config.mountOptions || {}
+  config.plugins = config.plugins || {}
+  return mount(EventList, {
+    global: {
+      plugins: [store, router]
+    },
+    ...config.mountOptions
+  })
+}
+
 describe('EventList', () => {
   it('should render the events', () => {
-    const wrapper = mount(EventList, {
-      global: {
-        plugins: [store, router]
-      }
-    })
+    const wrapper = mountEventList()
     expect(wrapper.exists()).toBeTruthy()
   })
 
   describe('page title', () => {
     it('is rendered with the correct text', () => {
-      const wrapper = mount(EventList, {
-        global: {
-          plugins: [store, router]
-        }
-      })
+      const wrapper = mountEventList()
       const title = wrapper.find('[data-testid=event-list-title]')
       expect(title.exists()).toBeTruthy()
       expect(title.text()).toContain('Events for Good')
     })
   })
   
-  describe('events', () => {
+  xdescribe('events', () => {
     it('are rendered in list', () => {
-      const wrapper = mount(EventList, {
-        global: {
-          plugins: [store, router]
-        }
-      })
+      const wrapper = mountEventList()
       const events = wrapper.findAll('[data-testid=event]')
       expect(events).toHaveLength(3)
     })
