78beb6c93d61c1d0d0d6c03e429d8b1d3f5fb861
Andy <andy@Andys-MacBook-Pro.local>
2021-04-21 23:02:02 -0400

    test page title

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -1,5 +1,5 @@
 <template>
-  <h1>Events for Good</h1>
+  <h1 data-testid="event-list-title">Events for Good</h1>
   <div class="events">
     <router-link
       class="event-link"
--- tests/unit/EventList.spec.js
+++ tests/unit/EventList.spec.js
@@ -12,4 +12,17 @@ describe('EventList', () => {
     })
     expect(wrapper.exists()).toBeTruthy()
   })
+
+  describe('page title', () => {
+    it('is rendered with the correct text', () => {
+      const wrapper = mount(EventList, {
+        global: {
+          plugins: [store, router]
+        }
+      })
+      const title = wrapper.find('[data-testid=event-list-title]')
+      expect(title.exists()).toBeTruthy()
+      expect(title.text()).toContain('Events for Good')
+    })
+  })
 })
